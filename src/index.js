d36e5846a3f931b4761e52fde2425a951b85b5e4
Rian Breno Polonini <rian.breno@injunior.com.br>
2021-09-11 11:03:49 -0300

    Cadastro de cliente feito

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const {v4: uuid4 } = require('uuid')
+
 const app = express();
 
-app.listen(3333);
+app.use(express.json());
 
-app.get("/", (req, resp) => {
-    resp.json({mensagem: "Olá"})
+const dados = []
+
+app.post("/account", (req, resp) => {
+    const { cpf, name} = req.body;
+
+    const cpfExists = dados.some(
+        (dados) => dados.cpf === cpf
+    );
+
+    if(cpfExists){
+        return resp.status(400).json({ error: "CPF já cadastrado"})
+    }
+
+    dados.push({
+        cpf,
+        name,
+        id: uuid4(),
+        statement: []
+    });
+    return resp.status(201).send("Usuário cadastrado com sucesso");
 })
 
+app.listen(3333, (erro) => {
+    if(erro){
+        console.log("Deu ruim!");
+    }else{
+        console.log("Deu bom!");
+    }
+})
